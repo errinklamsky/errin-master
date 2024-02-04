aabdd646a8b5dfc27441c9a33086fbacb83cae2b
errinklamsky <errinklamsky@gmail.com>
2024-02-04 10:36:21 +0800

    fixed bing

--- plugins/utilities/bing.js
+++ plugins/utilities/bing.js
@@ -1,11 +1,7 @@
 exports.run = {
    usage: ['bing'],
-   use: 'prompt',
-<<<<<<< HEAD
+   use: 'query',
    category: 'ai',
-=======
-   category: 'utilities',
->>>>>>> c2d44cab25bf04884e1d11f6dc73bfd5090923ad
    async: async (m, {
       client,
       text,
@@ -14,23 +10,13 @@ exports.run = {
       Func
    }) => {
       try {
-<<<<<<< HEAD
-         if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'hi'), m)
+         if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'apa itu kucing'), m)
          client.sendReact(m.chat, '🕒', m.key)
          const json = await Api.neoxr('/bing-chat', {
             q: text
          })
          if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
          client.reply(m.chat, json.data.message, m)
-=======
-            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'apa itu kucing'), m)
-            client.sendReact(m.chat, '🕒', m.key)
-            const json = await Api.neoxr('/bing-chat', {
-               q: text
-            })
-            if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
-            client.reply(m.chat, json.data.message, m)
->>>>>>> c2d44cab25bf04884e1d11f6dc73bfd5090923ad
       } catch (e) {
          client.reply(m.chat, Func.jsonFormat(e), m)
       }
@@ -39,8 +25,4 @@ exports.run = {
    limit: true,
    cache: true,
    location: __filename
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> c2d44cab25bf04884e1d11f6dc73bfd5090923ad
